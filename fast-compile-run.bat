9cfd4aa51401a8a2a82a4b33d7e6ac496144d0f2
Amayun <amayun.houery@e.rascol.net>
2018-04-23 14:55:40 +0200

    Updated compile batch script

--- fast-compile-run.bat
+++ fast-compile-run.bat
@@ -7,10 +7,18 @@ REM                                                     REM
 REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
 
 :init
-@echo off
+
+REM ---------- Organisation des fichiers ---------- LES NOMS DE DOSSIER DOIVENT ÊTRE CONFORMES (il DOIT y avoir un '/' à la fin)
+REM Nom du fichier de sortie
+set out=git-shrt.exe
+REM Nom du dossier contenant les fichiers sources
+set src=src/
+REM Nom du dossier contenant l'executable compilé (répertoire de sortie) (laisser vide pour repertoire courant)
+set bin=bin/
+
+echo off
 title Fast compile + run
 color 0f
-set out=git-shrt.exe
 cls
 mode con cols=96 lines=98302
 goto search
@@ -18,33 +26,54 @@ goto search
 :search
 	set cn=0
 	echo.
-	echo Preparation des fichiers
-	for %%F in (src/*.cpp) do (
+	echo Preparation des fichiers...
+	
+	IF NOT EXIST %src% (
+		echo Le dossier "%src%" suppose contenir les sources n'existe pas !
+		goto :exit
+	)
+	
+	for %%F in (%src%*.cpp) do (
 		set /a cn+=1
 		echo Fichier %%~nF selectionne
 	)
+	
 	IF %cn% EQU 0 (
 		echo Aucun fichier n'a ete trouve !
 		echo Fin du programme.
-		pause >nul
-		goto :eof
+		goto :exit
 	) ELSE (IF %cn% EQU 1 (
 		set s=
 	) ELSE (IF %cn% GTR 1 (
 		set s=s
 	)))
+	
 	echo Preparation terminee: %cn% fichier%s% trouve%s%
 	goto :compile
 
 :compile
+	REM Vérification du nom de dossier et création si non existant
+	if defined bin (
+		IF NOT EXIST %bin% mkdir %bin:~0,-1%
+	)
+	
+	REM Vérification du nom de l'executable qui sera créé
+	if defined out (
+		if "%out:~-4%" NEQ ".exe" (
+			echo Attention ! L'extension du fichier de sortie %out% n'est pas conforme a Windows, ce qui pourrait causer des problemes. Vous etes prevenus.
+		)
+	)
+	
 	echo.
 	echo.
 	echo ------ Compilation en cours...
 	echo.
+	
 	REM ----- Compilation -----
-	g++ src/*.cpp -o bin/%out% -Wall
+	g++ src/*.cpp -o %bin%%out% -Wall
 	set succ=%errorlevel%
 	REM -----    -----    -----
+	
 	echo.
 	IF %succ% EQU 0 (
 		echo ------ Compilation terminee !
@@ -60,7 +89,7 @@ goto search
 	echo Appuyez sur une touche pour lancer l'executable...
 	pause >nul
 	echo Execution de %out%
-	cd bin/
+	cd %bin%
 	start %out%
 	goto :eof
 	
