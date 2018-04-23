f4dbed3e15e7eb2a98d38278a4229d53ab7a7d1b
Amayun <amayun.houery@e.rascol.net>
2018-04-24 01:44:44 +0200

    Added some comments on test code

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,28 @@
 
 int main(int argc, char* args[])
 {
-	const std::string fileName = "test.txt";
-	
-    std::ifstream file;
+	const std::string fileName = "test.txt"; // Nom du fichier
+
+    std::ifstream file; // Ouverture du fichier
 	file.open(fileName);
-	
-	if (file)
-		std::cout << "Le fichier " << fileName << " s'est ouvert correctement" << std::endl;
-	else
+
+	// Test de l'ouverture du fichier
+	if (!file)
 	{
 		std::cout << "Une erreur est survenue lors de l'ouverture du fichier " << fileName;
 		return 1;
 	}
-	
+
 	std::cout << "Nous allons vous afficher le contenu du fichier, ligne par ligne:" << std::endl << std::endl;
-	
+
 	std::string line = "cc";
 	for (int l = 1; file >> line; l++)
 		std::cout << "Ligne " << l << ": " << line << std::endl;
-	
+
 	std::cout << std::endl << "Fin de la lecture du fichier !" << std::endl;
-	
+
 	file.close(); // Fermeture du fichier
-	
+
 	std::cout << std::endl << "Appuyez sur [Enter] pour continuer...";
 	std::cin.get();
 	return 0;
